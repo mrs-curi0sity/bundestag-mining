0117188f1b3807c04852495b2bb9ecd9e42fbc00
Magdalena Aretz <aretzlena@gmail.com>
2021-05-11 22:33:47 +0200

    inserted host in order for dashboard to be reachable from within docker container

--- dashboard/abgeordneten-dashboard.py
+++ dashboard/abgeordneten-dashboard.py
@@ -419,4 +419,4 @@ def update_feedback_records(n_clicks, page_current, page_size, start_date, end_d
 
 
 if __name__ == '__main__':
-    app.run_server(debug=True)
+    app.run_server(host="0.0.0.0",  port=8050, debug=True)
