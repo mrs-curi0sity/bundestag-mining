106f196bb9dc911ab88858f33c053f7d684a1d96
Magdalena Aretz <aretzlena@gmail.com>
2021-05-24 00:36:25 +0200

    major refactoring. fix of indexing problems

--- dashboard/abgeordneten-dashboard.py
+++ dashboard/abgeordneten-dashboard.py
@@ -2,6 +2,7 @@ import pandas as pd
 from pathlib import Path
 from datetime import datetime
 import logging
+import random
 
 import dash
 import dash_bootstrap_components as dbc
@@ -14,13 +15,9 @@ import dash_table
 # set loglevel to INFO. could also be DEBUG or WARNING or ERROR
 logging.getLogger().setLevel(logging.INFO)
 
-#app = dash.Dash()
-
-
 num_reloads = 0
 
-# define list of colors for traces here 
-LIST_OF_COLORS = ['#0ea46b', '#0e7da4', '#8bb5da', '#708292', '#1e61a0', '#101cb8', '#4e14c2', '#6d0ea4']
+LIST_OF_COLORS = ['#ff0000', '#000000', '#ffcc00', '#0066ff',  '#008000', '#ffa500', '#0000ff', '#4b0082', '#ee82ee', '#999999']
 LIST_OF_COLORS = LIST_OF_COLORS + LIST_OF_COLORS
 
 DATA_PATH = Path('../data')
@@ -29,12 +26,11 @@ DF_MDB_WP_PATH = DATA_PATH / 'df_mdb_wp.csv'
 df_mdb = pd.read_csv(DF_MDB_PATH)
 df_mdb_wp = pd.read_csv(DF_MDB_WP_PATH)
 
+
 MAX_WP = df_mdb_wp.WP.max()
 WP_START = [1949, 1953, 1957, 1961, 1965, 1969, 1972, 1976, 1980, 1983, 1987, 1990, 1994, 1998, 2002, 2005, 2009, 2013, 2017, 2021]
 
-# retrieve 8 most common parties (incl AFD and PDS, excluding DP, FU etc)
 
-# replace andere parteien with 'sonstige'
 def replace_sonstige(df_mdb, df_mdb_wp, dimension='PARTEI_KURZ', num_keep = 7):
     """
     keep num_keep most occurences, replace other values by "sonstige"
@@ -44,16 +40,16 @@ def replace_sonstige(df_mdb, df_mdb_wp, dimension='PARTEI_KURZ', num_keep = 7):
     values_to_keep = list(df_mdb_wp[['ID', dimension]].groupby(dimension).count().sort_values(by='ID', ascending=False)[:num_keep].index)
     values_to_discard = list(df_mdb[['ID', dimension]].groupby(dimension).count().sort_values(by='ID', ascending=False)[num_keep:].index)
     
-    logging.info(f'[{dimension}] replacing {values_to_discard} with <sonstige>')
+    logging.info(f'[{dimension}]. keeping {values_to_keep}. replacing {values_to_discard[:3]} ... with <sonstige>')
     df_mdb[dimension].replace(values_to_discard, 'sonstige', inplace=True)
     df_mdb_wp[dimension].replace(values_to_discard, 'sonstige', inplace=True)
-        
+    
     return values_to_keep, values_to_discard, df_mdb, df_mdb_wp
 
-list_of_parteien, list_of_parteien_discard, df_mdb, df_mdb_wp = replace_sonstige(df_mdb, df_mdb_wp, dimension='PARTEI_KURZ', num_keep = 8)
+list_of_parteien, list_of_parteien_discard, df_mdb, df_mdb_wp = replace_sonstige(df_mdb, df_mdb_wp, dimension='PARTEI_KURZ', num_keep = 7)
 list_of_religion, list_of_religion_discard, df_mdb, df_mdb_wp = replace_sonstige(df_mdb, df_mdb_wp, dimension='RELIGION', num_keep = 6) 
-list_of_familienstand, list_of_familienstand_discard, df_mdb, df_mdb_wp = replace_sonstige(df_mdb, df_mdb_wp, dimension='FAMILIENSTAND', num_keep = 6)
-list_of_beruf, list_of_beruf_discard, df_mdb, df_mdb_wp = replace_sonstige(df_mdb, df_mdb_wp, dimension='BERUF_MAPPED', num_keep = 15)
+list_of_familienstand, list_of_familienstand_discard, df_mdb, df_mdb_wp = replace_sonstige(df_mdb, df_mdb_wp, dimension='FAMILIENSTAND', num_keep = 7)
+list_of_beruf, list_of_beruf_discard, df_mdb, df_mdb_wp = replace_sonstige(df_mdb, df_mdb_wp, dimension='BERUF_MAPPED', num_keep = 10)
 
 # append 'sonstige' to list of valid values
 for list_of_values in [list_of_parteien, list_of_religion, list_of_familienstand, list_of_beruf]:
@@ -61,7 +57,7 @@ for list_of_values in [list_of_parteien, list_of_religion, list_of_familienstand
 
 # display List 
 PAGE_SIZE = 8
-COLUMNS_FOR_DISPLAY = ['NACHNAME', 'VORNAME', 'GEBURTSDATUM', 'PARTEI_KURZ', 'GESCHLECHT', 'FAMILIENSTAND', 'RELIGION', 'BERUF', 'BERUF_MAPPED', 'VEROEFFENTLICHUNGSPFLICHTIGES', 'VITA_KURZ']
+COLUMNS_FOR_DISPLAY = ['NACHNAME', 'VORNAME', 'GEBURTSDATUM', 'PARTEI_KURZ', 'FAMILIENSTAND', 'RELIGION', 'BERUF', 'BERUF_MAPPED', 'VEROEFFENTLICHUNGSPFLICHTIGES', 'VITA_KURZ'] #'GESCHLECHT'
     
     
 
@@ -105,11 +101,11 @@ controls = dbc.FormGroup(
         }),
         dcc.Dropdown(id='wp_start',
                      options=[{'label':str(WP_START[x-1]) + ' - ' + str(WP_START[x]), 'value': x} for x in range(1, MAX_WP+1)],
-                    value=7),
+                    value=1),
         
         dcc.Dropdown(id='wp_end',
                      options=[{'label':str(WP_START[x-1]) + ' - ' + str(WP_START[x]), 'value': x} for x in range(1, MAX_WP+1)],
-                    value=MAX_WP-1),
+                    value=MAX_WP),
         
         html.Br(),
         
@@ -119,7 +115,7 @@ controls = dbc.FormGroup(
             'textAlign': 'center'
         }),
         dbc.Card([dbc.Checklist(
-            id='check_list',
+            id='check_list_parteien',
             options=[{
             'label': partei,
             'value': partei
@@ -129,7 +125,31 @@ controls = dbc.FormGroup(
         )]),
         
         dbc.Button(
-            id='select_all',
+            id='select_all_parteien',
+            n_clicks=0,
+            children='Select All',
+            color='primary',
+            block=True
+        ),
+        html.Br(),
+        
+        
+        # checkbox list berufe
+        html.P('Berufe', style={
+            'textAlign': 'center'
+        }),
+        dbc.Card([dbc.Checklist(
+            id='check_list_berufe',
+            options=[{
+            'label': beruf,
+            'value': beruf
+                } for beruf in list_of_beruf],
+            value=[beruf for beruf in list_of_beruf],
+            # inline=True
+        )]),
+        
+        dbc.Button(
+            id='select_all_berufe',
             n_clicks=0,
             children='Select All',
             color='primary',
@@ -237,33 +257,42 @@ app.layout = html.Div([sidebar, content])
 # --------------------- callbacksf
 # click all parties
 @app.callback(
-    Output('check_list', 'value'),
-    [Input('select_all', 'n_clicks')],
-    [State('check_list', 'options'),
-     State('check_list', 'value')])
+    Output('check_list_parteien', 'value'),
+    [Input('select_all_parteien', 'n_clicks')],
+    [State('check_list_parteien', 'options'),
+     State('check_list_parteien', 'value')])
 def set_check_list_values(n_clicks, options, values):
     logging.info(f'N-CLICKS: {n_clicks}')
-    logging.info(f'Options: {options}')
-    logging.info(f'Options: {values}')
     if n_clicks%2 == 0:
-        logging.info(print('TRUE'))
         return [i['value'] for i in options]
     else:
         return []
-
-
-def select_data(start_date, end_date, selected_parteien, dimension='GESCHLECHT'):
-    # select wahlperiode
-    logging.info(f'+++++ starting evaluation of {dimension}')
-    selected_df = df_mdb_wp[(df_mdb_wp['WP']>= start_date) & (df_mdb_wp['WP']<= end_date)]
-    logging.info(f'fitting wp: {selected_df.shape}')
-    #logging.info(selected_df.head())
     
+
+# click all berufe
+@app.callback(
+    Output('check_list_berufe', 'value'),
+    [Input('select_all_berufe', 'n_clicks')],
+    [State('check_list_berufe', 'options'),
+     State('check_list_berufe', 'value')])
+def set_check_list_values(n_clicks, options, values):
+    logging.info(f'N-CLICKS: {n_clicks}')
+    if n_clicks%2 == 0:
+        return [i['value'] for i in options]
+    else:
+        return []    
+
+
+def select_data(start_date, end_date, selected_parteien, selected_berufe, dimension='GESCHLECHT'):
+    # select wahlperiode
+    selected_df = df_mdb_wp[(df_mdb_wp['WP']>= start_date) & (df_mdb_wp['WP']<= end_date)]
+
     # selct partei
     selected_df = selected_df[selected_df['PARTEI_KURZ'].isin(selected_parteien)]
-    logging.info(f'fitting parteien: {selected_df.shape}')
-    #logging.info(selected_parteien)
-    
+   
+    # select berufe
+    selected_df = selected_df[selected_df['BERUF_MAPPED'].isin(selected_berufe)]
+   
     grouped = selected_df[['ID', 'WP', dimension]].groupby([dimension, 'WP']).count()
     #grouped.reset_index(inplace=True)
     return grouped
@@ -282,7 +311,6 @@ def compute_traces(grouped, start_date, end_date, values_to_keep, dimension='GES
     grouped.reset_index(inplace=True)
     wps = sorted(list(set(grouped.WP)))
     years = [str(WP_START[wp]) for wp in wps]
-    logging.info(grouped.head())
 
     for value in values_to_keep:
         trace = grouped[grouped[dimension] == value].sort_values(by='WP').ID.values
@@ -302,10 +330,11 @@ def compute_traces(grouped, start_date, end_date, values_to_keep, dimension='GES
     [Input('submit_button', 'n_clicks')],
     [State('wp_start', 'value'),
      State('wp_end', 'value'),
-     State('check_list', 'value')
+     State('check_list_parteien', 'value'),
+    State('check_list_berufe', 'value')
      ])
-def update_graph_gender(n_clicks, start_date, end_date, selected_parteien):    
-    grouped = select_data(start_date, end_date, selected_parteien, dimension='GESCHLECHT')
+def update_graph_gender(n_clicks, start_date, end_date, selected_parteien, selected_berufe):    
+    grouped = select_data(start_date, end_date, selected_parteien, selected_berufe, dimension='GESCHLECHT')
     traces = compute_traces(grouped, start_date, end_date, ['männlich', 'weiblich'], dimension='GESCHLECHT')
     
     fig = {'data': traces,
@@ -321,11 +350,12 @@ def update_graph_gender(n_clicks, start_date, end_date, selected_parteien):
     [Input('submit_button', 'n_clicks')],
     [State('wp_start', 'value'),
      State('wp_end', 'value'),
-     State('check_list', 'value')
+     State('check_list_parteien', 'value'),
+    State('check_list_berufe', 'value')
      ])
-def update_graph_party(n_clicks, start_date, end_date, list_of_parteien):    
+def update_graph_party(n_clicks, start_date, end_date, selected_parteien, selected_berufe):    
 
-    grouped = select_data(start_date, end_date, list_of_parteien, dimension='PARTEI_KURZ')
+    grouped = select_data(start_date, end_date,  selected_parteien, selected_berufe, dimension='PARTEI_KURZ')
     traces = compute_traces(grouped, start_date, end_date, list_of_parteien, dimension='PARTEI_KURZ')
     
     fig = {'data': traces,
@@ -343,11 +373,12 @@ def update_graph_party(n_clicks, start_date, end_date, list_of_parteien):
     [Input('submit_button', 'n_clicks')],
     [State('wp_start', 'value'),
      State('wp_end', 'value'),
-     State('check_list', 'value')
+     State('check_list_parteien', 'value'),
+    State('check_list_berufe', 'value')
      ])
-def update_graph_religion(n_clicks, start_date, end_date, list_of_parteien):    
+def update_graph_religion(n_clicks, start_date, end_date, selected_parteien, selected_berufe):    
     
-    grouped = select_data(start_date, end_date, list_of_parteien, dimension='RELIGION')
+    grouped = select_data(start_date, end_date,  selected_parteien, selected_berufe, dimension='RELIGION')
     traces = compute_traces(grouped, start_date, end_date, list_of_religion, dimension='RELIGION')
     fig = {'data': traces,
         'layout': go.Layout(title = 'Religion')}
@@ -360,10 +391,11 @@ def update_graph_religion(n_clicks, start_date, end_date, list_of_parteien):
     [Input('submit_button', 'n_clicks')],
     [State('wp_start', 'value'),
      State('wp_end', 'value'),
-     State('check_list', 'value')
+     State('check_list_parteien', 'value'),
+    State('check_list_berufe', 'value')
      ])
-def update_graph_religion(n_clicks, start_date, end_date, list_of_parteien):    
-    grouped = select_data(start_date, end_date, list_of_parteien, dimension='FAMILIENSTAND')
+def update_graph_familienstand(n_clicks, start_date, end_date,  selected_parteien, selected_berufe):    
+    grouped = select_data(start_date, end_date,  selected_parteien, selected_berufe, dimension='FAMILIENSTAND')
     traces = compute_traces(grouped, start_date, end_date, list_of_familienstand, dimension='FAMILIENSTAND')
     fig = {'data': traces,
         'layout': go.Layout(title = 'Familienstand')}
@@ -377,11 +409,13 @@ def update_graph_religion(n_clicks, start_date, end_date, list_of_parteien):
     [Input('submit_button', 'n_clicks')],
     [State('wp_start', 'value'),
      State('wp_end', 'value'),
-     State('check_list', 'value')
+     State('check_list_parteien', 'value'),
+    State('check_list_berufe', 'value')
      ])
-def update_graph_religion(n_clicks, start_date, end_date, list_of_parteien):    
-    grouped = select_data(start_date, end_date, list_of_parteien, dimension='BERUF_MAPPED')
+def update_graph_beruf(n_clicks, start_date, end_date, selected_parteien, selected_berufe):    
+    grouped = select_data(start_date, end_date, selected_parteien, selected_berufe, dimension='BERUF_MAPPED')
     traces = compute_traces(grouped, start_date, end_date, list_of_beruf, dimension='BERUF')
+    #fig = go.Figure(data=[go.Scatter(traces)])#x=[1, 2, 3], y=[4, 1, 2])])
     fig = {'data': traces,
         'layout': go.Layout(title = 'Beruf')}
     return fig
@@ -397,15 +431,20 @@ def update_graph_religion(n_clicks, start_date, end_date, list_of_parteien):
      Input('selected_records', 'page_size')],
     [State('wp_start', 'value'),
      State('wp_end', 'value'), 
-     State('check_list', 'value')
+     State('check_list_parteien', 'value'),
+    State('check_list_berufe', 'value')
      ])
-def update_feedback_records(n_clicks, page_current, page_size, start_date, end_date, selected_parteien):
+def update_feedback_records(n_clicks, page_current, page_size, start_date, end_date, selected_parteien, selected_berufe):
     
     # select wahlperiode
     selected_df = df_mdb_wp[(df_mdb_wp['WP']>= start_date) & (df_mdb_wp['WP']<= end_date)]
     
     # selct partei
     selected_df = selected_df[selected_df['PARTEI_KURZ'].isin(selected_parteien)][COLUMNS_FOR_DISPLAY].drop_duplicates().sort_values(by='NACHNAME')#VEROEFFENTLICHUNGSPFLICHTIGES
+    
+    # select berufe
+    selected_df = selected_df[selected_df['BERUF_MAPPED'].isin(selected_berufe)]
+    
     logging.info(f'selected {selected_df.shape} many entries')
     
     return selected_df.iloc[
